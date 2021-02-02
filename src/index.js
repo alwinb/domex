7b1d71501332171b99a4f1dee88c00c3c61f9ac0
Alwin Blok <alwinb@users.noreply.github.com>
2021-02-02 15:06:36 +0100

    Prepere wrapping up; remove previous version

--- src/index.js
+++
@@ -1,4 +0,0 @@
-const log = console.log.bind (console)
-const { DomExImpl } = require ('./domex')
-const { createElement, createTextNode } = require ('./dom')
-module.exports = DomExImpl (createElement, createTextNode)
